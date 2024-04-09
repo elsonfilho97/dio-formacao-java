97f711a1fb1bafa792977bef3f9339a66ba025b8
Elson Filho <elson.filho@gmail.com>
2024-04-09 13:40:23 -0300

    Fim do módulo de condicionais.

---
+++ apps_java/src/edu/easf/04quartasemana/ControleFluxo/src/PlanoTelefonico.java
@@ -0,0 +1,23 @@
+public class PlanoTelefonico {
+
+public static void main(String[] args) {
+    String plano = "M"; //B, M ou T
+
+        switch (plano){
+
+            case "T":{
+                System.out.println("5GB para YouTube");
+            }
+        
+            case "M":{
+                System.out.println("WhatsApp e Instagram grátis");
+            }
+            
+            case "B":{
+                System.out.println("100 miutos de ligação");
+            }
+            
+            }
+    }   
+    
+}
