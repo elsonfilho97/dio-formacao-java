ec12257f58ca655d2255b26d76dc5e45167756e1
Elson Filho <elson.filho@gmail.com>
2024-04-09 13:38:17 -0300

    Fim do módulo de condicionais.

--- apps_java/src/edu/easf/04quartasemana/ControleFluxo/src/ResultadoEscolar.java
+++ apps_java/src/edu/easf/04quartasemana/ControleFluxo/src/ResultadoEscolar.java
@@ -1,15 +1,18 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 5;
+        int nota = 10;
 
-        if(nota >= 7)
-        System.out.println("Aprovado");
+        // if(nota >= 7)
+        // System.out.println("Aprovado");
 
-        else if(nota >=5 && nota < 7)
-        System.out.println("Recuperação");
+        // else if(nota >=5 && nota < 7)
+        // System.out.println("Recuperação");
 
-        else
-        System.out.println("Reprovado");
+        // else
+        // System.out.println("Reprovado");
+
+        int resultado = nota >= 7 ? 10 : nota >= 5 && nota < 7 ? 20:30;
+        System.out.println(resultado);
 
     }
 }
